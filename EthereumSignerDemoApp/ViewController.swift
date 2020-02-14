0518a53485cd634a66465c03b10ffa304fa65dba
Apple Imac <appleimac@Apples-iMac.domain.name>
2020-02-14 11:51:15 +0530

    first commit

---
+++ EthereumSignerDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EthereumSignerDemoApp
+//
+//  Created by Apple Imac on 14/02/20.
+//  Copyright © 2020 Self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
